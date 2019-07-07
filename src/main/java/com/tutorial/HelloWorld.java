f602ed4fe677310914406f6f92469de76ba31bcc
zdw <zdw_mzsf@163.com>
2019-07-07 11:14:43 +0800

    add new function

--- src/main/java/com/tutorial/HelloWorld.java
+++ src/main/java/com/tutorial/HelloWorld.java
@@ -1,7 +1,32 @@
 package com.tutorial;
 
+import java.io.*;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        String welcomString = System.getProperties().getProperty("welcomeString");
+        String titles = System.getProperties().getProperty("titles");
+        String[] titleArr = titles.split(",");
+
+
+        FileWriter writer = new FileWriter(new File("test.md"));
+        writer.write(welcomString);
+        writeTripuleLine(writer);
+
+        for (String title : titleArr) {
+            writer.write("## " + title);
+            writeTripuleLine(writer);
+        }
+
+        writer.flush();
+        writer.close();
+
+
+    }
+
+    private static void writeTripuleLine(FileWriter writer) throws IOException {
+        writer.write("\n");
+        writer.write("\n");
+        writer.write("\n");
     }
 }
